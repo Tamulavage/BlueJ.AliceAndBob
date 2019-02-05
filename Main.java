3667dc80047ef5f2e30d5c0c420b8b1f28c3e8cb
David Tamulavage <davidtamulavage@zipcoders-MacBook-Pro.local>
2019-02-05 09:40:31 -0500

    Prompt names and validate

--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+// import scanner 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = reader.next();
+        
+        // close reader
+        reader.close();
+        
+        // testing only to ensure reading name - comment out after test
+        //System.out.println(name);
+        
+        // ensure to use equals method
+        if(name.equals("Alice")|| name.equals("Bob"))
+        {
+            System.out.println("Hello " + name + ". Would you like a coffee?");
+            
+        }
 
+        
     }
 }
